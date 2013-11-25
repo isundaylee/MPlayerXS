58d87e20e8e9cee456f5e600c8c55f51635702d5
Jiahao Li <=>
2013-11-25 17:55:08 -0500

    Increases the speed of speed change.

--- MPlayerX/ShortCutManager.m
+++ MPlayerX/ShortCutManager.m
@@ -39,7 +39,7 @@
 {
 	[[NSUserDefaults standardUserDefaults] 
 	 registerDefaults:[NSDictionary dictionaryWithObjectsAndKeys:
-					   [NSNumber numberWithFloat:0.1], kUDKeySpeedStep,
+					   [NSNumber numberWithFloat:0.3], kUDKeySpeedStep,
 					   [NSNumber numberWithFloat:10], kUDKeySeekStepLR,
 					   [NSNumber numberWithFloat:60], kUDKeySeekStepUB,
 					   [NSNumber numberWithFloat:0.1], kUDKeySubDelayStepTime,
